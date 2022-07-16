1c22498a38cf366cb60a318293d1e4e10606b7d8
Gustavo La Madrid Coz <104698981+12LaMadrid12@users.noreply.github.com>
2022-07-16 17:30:39 -0500

    ADD CONFIG SERVER

---
+++ src/main/java/com/unmsm/matchjobs/configserver/MatchjobsConfigApplication.java
@@ -0,0 +1,15 @@
+package com.unmsm.matchjobs.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MatchjobsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchjobsConfigApplication.class, args);
+	}
+
+}
